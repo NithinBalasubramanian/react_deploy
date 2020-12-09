1decdd468c8a2c1db1d255e6ad3c063830582a9f
NithinBalasubramanian <nithinmigocrogle22@outlook.com>
2020-12-09 09:47:13 +0530

    Updates

---
+++ precache-manifest.2dc4310223eebf58f62fa8ea0d82013e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dbea980b9ee137f6cf48a69445c79146",
+    "url": "/react_deploy/index.html"
+  },
+  {
+    "revision": "1d5203cf5470393fc67e",
+    "url": "/react_deploy/static/css/2.3cf5d70f.chunk.css"
+  },
+  {
+    "revision": "df736b35306ee28e1088",
+    "url": "/react_deploy/static/css/main.2a179be6.chunk.css"
+  },
+  {
+    "revision": "1d5203cf5470393fc67e",
+    "url": "/react_deploy/static/js/2.cc054bc6.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/react_deploy/static/js/2.cc054bc6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "df736b35306ee28e1088",
+    "url": "/react_deploy/static/js/main.ae9b7627.chunk.js"
+  },
+  {
+    "revision": "b004444f852ffad6b9e4",
+    "url": "/react_deploy/static/js/runtime-main.3e41fb6d.js"
+  }
+]);
